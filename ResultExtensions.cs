e0a585cb72fb40204b9f4f56b34c4d28f67d413c
Real Matrix <noel.ar@protonmail.com>
2024-04-09 16:54:45 +0100

    Update ResultExtensions.cs

--- ResultExtensions.cs
+++ ResultExtensions.cs
@@ -1,8 +1,8 @@
-#region with use of prerequisite MediatR coupled with CQRS, i.e  
+#region with use of prerequisite MediatR coupled with CQRS, i.e
 public interface IQuery<TR> : IRequest<Result<TR>> {}
+public interface IQueryHandler<TQ, TR> : IRequestHandler<TQ, Result<TR>> where TQ : IQuery<TR> {}
 public interface ICommand<TR> : IRequest<Result<TR>> {}
 public interface ICommandHandler<TC, TR> : IRequestHandler<TC, Result<TR>> where TC : ICommand<TR> {}
-public interface IQueryHandler<TQ, TR> : IRequestHandler<TQ, Result<TR>> where TQ : IQuery<TR> {}
 #endregion
 internal static sealed class ResultExtensions 
 {
@@ -28,3 +28,10 @@ public async Task<IActionResult> UpdateMember(
 // [..]
 return await Result.Create(new UpdateUserCommand(id, req.FirstName, req.LastName)
     .Bind(cmd => Sender.Send(cmd, ctk)).Match(() => NoContent(), result => HandleFailure());
+
+                           
+                           /** non-generic version:
+public sealed record CreateCommand(args) : IRequest;
+public sealed record GetUserByIdQuery(args) : IRequest;
+internal sealed class CreateCommandHandler : IRequestHandler<CreateCommand> [...]
+internal sealed class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery> [...] **/
