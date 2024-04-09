436f4206109f69b5abda1a999325a345f8aee778
Real Matrix <noel.ar@protonmail.com>
2024-04-09 15:37:48 +0100

    Update ResultExtensions.cs

--- ResultExtensions.cs
+++ ResultExtensions.cs
@@ -1,12 +1,13 @@
-#region     with use of MediatR coupled with CQRS, i.e  
-public interface ICommandHandler<TC> : IRequestHandler<TC, Result> where TC : ICommand {}
+#region with use of prerequisite MediatR coupled with CQRS, i.e  
+public interface ICommandHandler<TC> : IRequestHandler<TC, Result>
+    where TC : ICommand
+{
+}
 public interface ICommandHandler<TC, TResponse> : IRequestHandler<TC, Result<TResponse>> where TC : ICommand<TResponse> {}
 #endregion
-
 internal static sealed class ResultExtensions 
 {
-    internal static async Task<Result<TOut>> Bind<TIn, TOut>(
-        this Result<TIn> res, Func<TIn, Task<Result<TOut>>> func)
+    internal static async Task<Result<TOut>> Bind<TIn, TOut>(this Result<TIn> res, Func<TIn, Task<Result<TOut>>> func)
     {
         return res.IsFailure ? res.Failure<TOut>(res.Errors) : await func(res.Value);
     }
