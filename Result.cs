859130f4a87adb289ffe0c0c2eef84855b8ea73e
Real Matrix <noel.ar@protonmail.com>
2024-05-15 12:12:22 +0100

    Update Result.cs

--- Result.cs
+++ Result.cs
@@ -1,7 +1,7 @@
 public class Error : IEquatable<Error>
 {
-    public static readonly Error None = new(string.Empty, string.Empty);
     public static readonly Error NullValue = new("Error.NullValue", "The specified result value is null.");
+    public static readonly Error None = new(string.Empty, string.Empty);
     
     public Error(string code, string message)
     {
