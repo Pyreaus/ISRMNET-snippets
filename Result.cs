69ebc67405262b376ba0559518dbe20b81e5cc0f
Real Matrix <noel.ar@protonmail.com>
2024-05-15 13:07:27 +0100

    Update Result.cs

--- Result.cs
+++ Result.cs
@@ -31,3 +31,16 @@ public class Result
 
     public static Result<TValue> Create<TValue>(TValue? value) => value != null ? Success(value) : Failure<TValue>(Error.NullValue);
 }
+// generic Result<T> class
+
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    protected internal Result(TValue? value, bool isSuccess, Error error) : base(isSuccess, error) => _value = value;
+
+    public static implicit operator Result<TValue>(TValue? value) => Create(value);
+    
+    public TValue Value => IsSuccess ? _value! 
+    : throw new InvalidOperationException("The value of a failure result can not be accessed.");
+}
