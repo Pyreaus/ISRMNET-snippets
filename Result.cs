858f2bcfbad6dc99e4dcd2c8f05774f6d668a8f8
Real Matrix <noel.ar@protonmail.com>
2024-05-15 13:03:52 +0100

    Update Result.cs
    reformatted

--- Result.cs
+++ Result.cs
@@ -15,17 +15,18 @@ public class Result
         (IsSuccess, Error) = (isSuccess, error);
     }
 
-    public bool IsSuccess { get; }
-
-    public bool IsFailure => !IsSuccess;
 
     public Error Error { get; }
+    
+    public bool IsFailure => !IsSuccess;
 
-    public static Result<TValue> Failure<TValue>(Error error) => new(default, false, error);
+    public bool IsSuccess { get; }
 
+    public static Result Success() => new(true, Error.None);
+    
     public static Result Failure(Error error) => new(false, error);
     
-    public static Result Success() => new(true, Error.None);
+    public static Result<TValue> Failure<TValue>(Error error) => new(default, false, error);
 
     public static Result<TValue> Success<TValue>(TValue value) => new(value, true, Error.None);
 
