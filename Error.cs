9c47c01390d0f9e6b2983eaf894a1a90714ddcc1
Real Matrix <noel.ar@protonmail.com>
2024-05-16 15:59:31 +0100

    Update Error.cs

--- Error.cs
+++ Error.cs
@@ -1,42 +1,46 @@
-public class Error(string code, string message) : IEquatable<Error>
+namespace ISRM.isrmnet.DAL.Shared
 {
-    public static readonly Error NullValue = new(Errors.NullValue, "The specified result value is null.");
-    public static readonly Error None = new(string.Empty, string.Empty);
-
-    public static implicit operator Result(Error e) => Result.Failure(e);
-    public static implicit operator string(Error e) => e.Code;
-    
-    public string Code { get; } = code;
-    public string Message { get; } = message;
-    
-    public static bool operator ==(Error? a, Error? b)
+    public class Error(string code, string message) : IEquatable<Error>
     {
-        if (a is null && b is null)
+        public static readonly Error NullValue = new(Errors.NullValue, "The specified result value is null.");
+        public static readonly Error None = new(string.Empty, string.Empty);
+    
+        public static implicit operator Result(Error e) => Result.Failure(e);
+        public static implicit operator string(Error e) => e.Code;
+        
+        public string Code { get; } = code;
+        public string Message { get; } = message;
+        
+        public static bool operator ==(Error? a, Error? b)
         {
-            return true;
-        }
-
-        else if (a is null || b is null)
+            if (a is null && b is null)
+            {
+                return true;
+            }
+    
+            else if (a is null || b is null)
+            {
+                return false;
+            }
+    
+            return a.Equals(b);
+        }   
+        public static bool operator !=(Error? a, Error? b) => !(a == b);
+    
+        public virtual bool Equals(Error? other)
         {
-            return false;
+            if (other is null)
+            {
+                return false;
+            }
+    
+            return Code == other.Code && Message == other.Message;
         }
-
-        return a.Equals(b);
-    }   
-    public static bool operator !=(Error? a, Error? b) => !(a == b);
-
-    public virtual bool Equals(Error? other)
-    {
-        if (other is null)
-        {
-            return false;
-        }
-
-        return Code == other.Code && Message == other.Message;
+        public override bool Equals(object? obj) => obj is Error error && Equals(error);
+        
+        public override string ToString() => Code;
+        
+        public override int GetHashCode() => HashCode.Combine(Code, Message);
     }
-    public override bool Equals(object? obj) => obj is Error error && Equals(error);
-    
-    public override string ToString() => Code;
-    
-    public override int GetHashCode() => HashCode.Combine(Code, Message);
 }
+
