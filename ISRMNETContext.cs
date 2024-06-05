ccbb75d2dc159d3a68ce60f6e7b6fa561b342078
Real Matrix <noel.ar@protonmail.com>
2024-06-05 18:41:17 +0100

    Update ISRMNETContext.cs

--- ISRMNETContext.cs
+++ ISRMNETContext.cs
@@ -3,7 +3,7 @@ using ISRM.isrmnet.Model.POCOs.Entities;
 /// <summary><para>
 ///  EF Core expects this class to always reside in DAL.dll, circumvent this by:
 ///  Explicitly specifying the assembly containing this class when performing migrations: (dotnet ef migrations add <MigrationName> --startup-project <RelativePathToAssembly>)
-///  --OR--  Implementing the IDesignTimeDbContextFactory<ISRMNETContext> interface in ISRMNETDbFactory or in a seperate class.  
+///  --OR--  Implementing the <see cref="IDesignTimeDbContextFactory{ISRMNETContext}"/> interface in <c>ISRMNETDbFactory</c> or in a seperate class.  
 /// <summary><para>
 
 namespace ISRM.isrmnet.Model.Contexts
